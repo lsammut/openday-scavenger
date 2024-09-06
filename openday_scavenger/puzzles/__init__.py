d7a0d93111909332e4a31ca8cdc8383f28e6c9c2
Letizia Sammut <letizias@ansto.gov.au>
2024-09-06 20:39:00 +1000

    Add crossword generator page

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from .demo.views import router as puzzle_demo_router
+from .crossword_generator.views import router as crossword_generator_router
 
 
 router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
 router.include_router(puzzle_demo_router, prefix='/demo')
+router.include_router(crossword_generator_router, prefix='/crossword_generator')
